c034d9e0c9f60f272ecb1f3f6dba5669fdc143d4
unknown <npduy124@gmail.com>
2023-06-07 20:31:08 +0700

    123

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
-const {dataDieuXe} = require('./dieuxe.js');
-const {xulyDataDieuXe} = require('./help/functions.js');
-require('dotenv').config();
+const { dataDieuXe } = require("./dieuxe.js");
+const { xulyDataDieuXe } = require("./help/functions.js");
+require("dotenv").config();
 
 var low = require("lowdb");
 var FileSync = require("lowdb/adapters/FileSync");
@@ -23,16 +23,28 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.get("/", (req, res) => {
   res.render("index", { title: "Hey", message: "Hello there!" });
 });
+app.get("/tinh", (req, res) => {
+  res.render("index");
+});
+app.post("/tinh", (req, res) => {
+  let a = parseInt(req.body["6kg"]) || 0;
+  let b = parseInt(req.body["12kg"]) || 0;
+  let c = parseInt(req.body["12.5kg"]) || 0;
+  let d = parseInt(req.body["45kg"]) || 0;
+  let e = parseInt(req.body["50kg"]) || 0;
+  console.log(a);
+  let tongTrongLuong = a * 15 + b * 25 + c * 25 + d * 85 + e * 90;
+  console.log(tongTrongLuong);
+  res.render("index", { tongTrongLuong });
+});
 
-
-app.get('/dieuxe', (req, res)=>{
-    let thongTinDieuXe = xulyDataDieuXe(dataDieuXe);
-    res.render('dieuxe/index', {
-      data : thongTinDieuXe
-    })
-    // res.json(thongTinDieuXe)
-})
-
+app.get("/dieuxe", (req, res) => {
+  let thongTinDieuXe = xulyDataDieuXe(dataDieuXe);
+  res.render("dieuxe/index", {
+    data: thongTinDieuXe,
+  });
+  // res.json(thongTinDieuXe)
+});
 
 app.get("/users", (req, res) => {
   res.render("users/index", {
@@ -55,6 +67,10 @@ app.get("/users/search", (req, res) => {
     users: matchedUsers,
   });
 });
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });

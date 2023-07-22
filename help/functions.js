9f62f33fbbe40e2eecb16c59aa30323a99bc45ca
unknown <npduy124@gmail.com>
2023-07-22 19:31:20 +0700

    xuat-kho

--- help/functions.js
+++ help/functions.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 function sortDieuXe(xe, list) {
   let sortable = [];
   let tmp = {};
@@ -53,8 +55,137 @@ function xulyDataDieuXe(dataDieuXe) {
     let sort = sortDieuXe(xe, result[xe]);
     result[xe] = sort;
   });
-  
+
   return result;
 }
 
-module.exports = { xulyDataDieuXe };
+function gopDonDaiLy(tong_don_theo_so_phieu) {
+  let first_don_hang = tong_don_theo_so_phieu[0];
+  let ma_so_phieu = first_don_hang["Mã số phiếu"];
+  let ten_khach_hang = first_don_hang["Khách hàng"];
+  let tong_so_luong = first_don_hang["Tổng số lượng"];
+  let details = {
+    _6kg: 0,
+    _12kg: 0,
+    _12kg5: 0,
+    _45kg: 0,
+    _50kg: 0,
+  };
+  let tong_so_binh_quet = 0;
+  _.forEach(tong_don_theo_so_phieu, function (o) {
+    switch (o["Dung lượng"]) {
+      case "6kg":
+        details["_6kg"] += 1;
+        break;
+      case "12kg":
+        details["_12kg"] += 1;
+        break;
+      case "12.5kg":
+        details["_12kg5"] += 1;
+        break;
+      case "45kg":
+        details["_45kg"] += 1;
+        break;
+      case "50kg":
+        details["_50kg"] += 1;
+        break;
+      default:
+        break;
+    }
+  });
+
+  tong_so_binh_quet = Object.values(details).reduce((a, b) => a + b, 0);
+  return {
+    ma_so_phieu,
+    ten_khach_hang,
+    tong_so_luong,
+    details,
+    tong_so_binh_quet,
+  };
+}
+
+function gopKhachHangTrungTen(danh_sach_don_hang) {
+  let ten_khach_hang_arr = [];
+  let clone_dsdh = _.cloneDeep(danh_sach_don_hang);
+  _.forEach(danh_sach_don_hang, function (o) {
+    if (ten_khach_hang_arr.indexOf(o["ten_khach_hang"]) < 0) {
+      ten_khach_hang_arr.push(o["ten_khach_hang"]);
+    }
+  });
+
+  let result = [];
+  _.forEach(ten_khach_hang_arr, function (ten_khach_hang, i) {
+    let tmp = _.filter(clone_dsdh, function (o) {
+      return o["ten_khach_hang"] == ten_khach_hang;
+    });
+    if (tmp.length > 1) {
+      let tsl = 0;
+      let sum_details = { _6kg: 0, _12kg: 0, _12kg5: 0, _45kg: 0, _50kg: 0 };
+      let tsbq = 0;
+      _.forEach(tmp, function (obj) {
+        tsl += parseInt(obj["tong_so_luong"]);
+        tsbq += parseInt(obj["tong_so_binh_quet"]);
+        sum_details["_6kg"] += parseInt(obj["details"]["_6kg"]);
+        sum_details["_12kg"] += parseInt(obj["details"]["_12kg"]);
+        sum_details["_12kg5"] += parseInt(obj["details"]["_12kg5"]);
+        sum_details["_45kg"] += parseInt(obj["details"]["_45kg"]);
+        sum_details["_50kg"] += parseInt(obj["details"]["_50kg"]);
+      });
+      tmp = {
+        ten_khach_hang: tmp[0]["ten_khach_hang"],
+        tong_so_luong: tsl,
+        details: sum_details,
+        tong_so_binh_quet: tsbq,
+        dia_chi_dai_ly: tmp[0]["dia_chi_dai_ly"],
+      };
+      result.push(tmp);
+    } else {
+      result.push(tmp[0]);
+    }
+  });
+  return result;
+}
+
+
+function gopDonTheoKhuVuc(danh_sach_don_hang) {
+  let ten_khu_vuc_arr = [];
+  let clone_dsdh = _.cloneDeep(danh_sach_don_hang);
+  _.forEach(danh_sach_don_hang, function (o) {
+    if (ten_khu_vuc_arr.indexOf(o["dia_chi_dai_ly"]) < 0) {
+      ten_khu_vuc_arr.push(o["dia_chi_dai_ly"]);
+    }
+  });
+
+  let result = [];
+  _.forEach(ten_khu_vuc_arr, function (ten_khu_vuc, i) {
+    let tmp = _.filter(clone_dsdh, function (o) {
+      return o["dia_chi_dai_ly"] == ten_khu_vuc;
+    });
+    if (tmp.length > 1) {
+      let tsl = 0;
+      let sum_details = { _6kg: 0, _12kg: 0, _12kg5: 0, _45kg: 0, _50kg: 0 };
+      let tsbq = 0;
+      _.forEach(tmp, function (obj) {
+        tsl += parseInt(obj["tong_so_luong"]);
+        tsbq += parseInt(obj["tong_so_binh_quet"]);
+        sum_details["_6kg"] += parseInt(obj["details"]["_6kg"]);
+        sum_details["_12kg"] += parseInt(obj["details"]["_12kg"]);
+        sum_details["_12kg5"] += parseInt(obj["details"]["_12kg5"]);
+        sum_details["_45kg"] += parseInt(obj["details"]["_45kg"]);
+        sum_details["_50kg"] += parseInt(obj["details"]["_50kg"]);
+      });
+      tmp = {
+        dia_chi_dai_ly: tmp[0]["dia_chi_dai_ly"],
+        tong_so_luong: tsl,
+        details: sum_details,
+        tong_so_binh_quet: tsbq,
+      };
+      result.push(tmp);
+    } else {
+      result.push(tmp[0]);
+    }
+  });
+  return result;
+}
+
+module.exports = { xulyDataDieuXe, gopDonDaiLy, gopKhachHangTrungTen, gopDonTheoKhuVuc };
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+const fs = require("fs");
+const csv = require("fast-csv");
+const _ = require("lodash");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
 const { dataDieuXe } = require("./dieuxe.js");
-const { xulyDataDieuXe } = require("./help/functions.js");
+const {
+  xulyDataDieuXe,
+  gopDonDaiLy,
+  gopKhachHangTrungTen,
+  gopDonTheoKhuVuc,
+} = require("./help/functions.js");
+
 require("dotenv").config();
 
 var low = require("lowdb");
 var FileSync = require("lowdb/adapters/FileSync");
+const { log } = require("console");
 var adapter = new FileSync("db.json");
 var db = low(adapter);
 db.defaults({
@@ -38,6 +48,153 @@ app.post("/tinh", (req, res) => {
   res.render("index", { tongTrongLuong });
 });
 
+app.get("/don-hang", (req, res) => {
+  const data = [];
+  fs.createReadStream("./files/don-hang.csv")
+    .pipe(csv.parse({ headers: true, discardUnmappedColumns: true }))
+    .on("error", error => res.json(error))
+    .on("data", row => data.push(row))
+    .on("end", () => {
+      let donHangList = [
+        {
+          "Đai lý": "HÙNG HÀO PHÁT",
+          "Mã số hoá đơn": "5329",
+          "Tổng Số lượng": "239",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P XÁM",
+          "Số lượng": "230",
+        },
+        {
+          "Đai lý": "HÙNG HÀO PHÁT",
+          "Mã số hoá đơn": "5329",
+          "Tổng Số lượng": "239",
+          "Dung luợng": "45kg",
+          "Thương hiệu": "PC45",
+          "Số lượng": "9",
+        },
+        {
+          "Đai lý": "Ngọc ánh",
+          "Mã số hoá đơn": "5328",
+          "Tổng Số lượng": "80",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P Xám",
+          "Số lượng": "50",
+        },
+        {
+          "Đai lý": "Ngọc ánh",
+          "Mã số hoá đơn": "5328",
+          "Tổng Số lượng": "80",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P Xanh Đậm",
+          "Số lượng": "20",
+        },
+        {
+          "Đai lý": "Ngọc ánh",
+          "Mã số hoá đơn": "5328",
+          "Tổng Số lượng": "80",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P Shell",
+          "Số lượng": "10",
+        },
+        {
+          "Đai lý": "Ngọc Bích",
+          "Mã số hoá đơn": "5327",
+          "Tổng Số lượng": "100",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P Xám",
+          "Số lượng": "100",
+        },
+        {
+          "Đai lý": "Hải Âu",
+          "Mã số hoá đơn": "5326",
+          "Tổng Số lượng": "100",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P Xám",
+          "Số lượng": "90",
+        },
+        {
+          "Đai lý": "Hải Âu",
+          "Mã số hoá đơn": "5326",
+          "Tổng Số lượng": "100",
+          "Dung luợng": "45kg",
+          "Thương hiệu": "PC45",
+          "Số lượng": "10",
+        },
+        {
+          "Đai lý": "Trọng Thành",
+          "Mã số hoá đơn": "5325",
+          "Tổng Số lượng": "100",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P Xám",
+          "Số lượng": "100",
+        },
+        {
+          "Đai lý": "Ngọc ánh",
+          "Mã số hoá đơn": "5333",
+          "Tổng Số lượng": "80",
+          "Dung luợng": "12kg",
+          "Thương hiệu": "P Xám",
+          "Số lượng": "30",
+        },
+      ];
+
+      let cloneDonHang = _.cloneDeep(donHangList);
+      let result = [];
+      let orders = [];
+      for (let i = 0; i < cloneDonHang.length; i++) {
+        const element = cloneDonHang[i];
+        const maHoaDon = element["Mã số hoá đơn"];
+        if (orders.indexOf(maHoaDon) < 0) {
+          orders.push(maHoaDon);
+        }
+      }
+
+      orders.forEach(code => {
+        let tmp = cloneDonHang.filter(o => {
+          return o["Mã số hoá đơn"] == code;
+        });
+        let sumOrder = {
+          [code]: {
+            "Đại lý": tmp[0]["Đai lý"],
+            orders: [],
+          },
+        };
+
+        for (let i = 0; i < tmp.length; i++) {
+          const element = tmp[i];
+          sumOrder[code]["orders"].push({
+            "Dung luợng": element["Dung luợng"],
+            "Thương hiệu": element["Thương hiệu"],
+            "Số lượng": element["Số lượng"],
+          });
+        }
+        result.push(sumOrder);
+      });
+
+      let finalResult = [];
+      let daiLyList = [];
+
+      for (let index = 0; index < result.length; index++) {
+        const daiLy = result[index][Object.keys(result[index])[0]]["Đại lý"];
+        if (!daiLyList.includes(daiLy)) daiLyList.push(daiLy);
+      }
+      // console.log(daiLyList);
+      daiLyList.forEach(name => {
+        let obj = {};
+        let tmp = result.filter(ele => {
+          return ele[Object.keys(ele)[0]]["Đại lý"] == name;
+        });
+        // console.log(tmp);
+        obj[name] = {};
+        for (let i = 0; i < tmp.length; i++) {
+          console.log(tmp[i][Object.keys(tmp[i])]["orders"]);
+        }
+        // console.log(obj)
+      });
+      res.json(donHangList);
+    });
+});
+
 app.get("/dieuxe", (req, res) => {
   let thongTinDieuXe = xulyDataDieuXe(dataDieuXe);
   res.render("dieuxe/index", {
@@ -66,6 +223,63 @@ app.get("/users/search", (req, res) => {
   res.render("users/index", {
     users: matchedUsers,
   });
+});
+
+app.get("/xuat-kho", (req, res) => {
+  const data = [];
+  const danh_sach_dai_ly = [];
+  fs.createReadStream("./files/danh-sach-dai-ly.csv")
+    .pipe(csv.parse({ headers: true, discardUnmappedColumns: true }))
+    .on("error", error => res.json(error))
+    .on("data", row => danh_sach_dai_ly.push(row))
+    .on("end", () => {
+      console.log(danh_sach_dai_ly.length); // lấy danh sách đại lý
+      fs.createReadStream("./files/xuat-kho.csv") // bắt đầu lấy danh sách xuất kho
+        .pipe(csv.parse({ headers: true, discardUnmappedColumns: true }))
+        .on("error", error => res.json(error))
+        .on("data", row => data.push(row))
+        .on("end", () => {
+          let cloneDonHang = _.cloneDeep(data);
+          // lấy danh sách mã số phiếu
+          let ma_phieu_arr = [];
+          for (let i = 0; i < cloneDonHang.length; i++) {
+            const ma_phieu = cloneDonHang[i]["Mã số phiếu"];
+            if (ma_phieu_arr.indexOf(ma_phieu) < 0) {
+              ma_phieu_arr.push(ma_phieu);
+            }
+          }
+
+          // danh
+          let danh_sach_don_hang = [];
+          _.forEach(ma_phieu_arr, function (so_phieu) {
+            
+            let tong_don_theo_so_phieu = _.filter(cloneDonHang, function (o) {
+              // or remove
+              return o["Mã số phiếu"] == so_phieu;
+            });
+            let gop_don_hang = gopDonDaiLy(tong_don_theo_so_phieu);
+            let first_don_hang = tong_don_theo_so_phieu[0];
+            let ten_dai_ly = first_don_hang["Khách hàng"];
+            let dai_ly_obj = _.find(danh_sach_dai_ly, function (o) {
+              return o["Tên viết tắt"] == ten_dai_ly;
+            });
+
+            let dia_chi_dai_ly = dai_ly_obj[`Tỉnh/Thành phố`];
+            gop_don_hang["dia_chi_dai_ly"] = dia_chi_dai_ly;
+            danh_sach_don_hang.push(gop_don_hang);
+          });
+
+          let khach_hang_trung_ten_filter =
+            gopKhachHangTrungTen(danh_sach_don_hang);
+          let gop_don_theo_khu_vuc = gopDonTheoKhuVuc(
+            khach_hang_trung_ten_filter
+          );
+
+          res.render("xuat-kho/index", {
+            orders: gop_don_theo_khu_vuc,
+          });
+        });
+    });
 });
 app.listen(process.env.PORT || 3000, function () {
   console.log(
